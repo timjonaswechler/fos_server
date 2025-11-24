620dde301a2f656e77d4b460d49b5af4ea012e63
Tim-Jonas Wechler <code.wechlertimjonas@icloud.com>
2025-11-24 10:04:43 +0100

    refactoring bundling in a plugin

--- fos_server/src/bin/host.rs
+++ fos_server/src/bin/host.rs
@@ -1,100 +1,17 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin, EguiPrimaryContextPass};
-use rand::Rng; // Für Demo-Zufallsfehler
+use fos_server::*;
 
 fn main() -> AppExit {
     App::new()
-        .add_plugins((
-            DefaultPlugins,
-            EguiPlugin::default(),
-        ))
-        .init_resource::<FakeLoadingTimer>()
-        .init_resource::<ErrorMessage>() // Neue Resource für Fehlertexte
-        .init_state::<AppScope>()
-        .add_sub_state::<SingleplayerState>()
-        .add_sub_state::<OpenToLANState>()
-        .add_sub_state::<ConnectToServerState>()
+        .add_plugins((DefaultPlugins, EguiPlugin::default(), FOSServerPlugin))
         .add_systems(Startup, setup_camera_system)
         .add_systems(EguiPrimaryContextPass, ui_example_system)
-        
-        // --- Simulation Logic ---
-        .add_systems(Update, simulate_singleplayer_starting.run_if(in_state(SingleplayerState::Starting)))
-        .add_systems(Update, simulate_singleplayer_closing.run_if(in_state(SingleplayerState::Closing)))
-        
-        .add_systems(Update, simulate_going_public.run_if(in_state(OpenToLANState::GoingPublic)))
-        .add_systems(Update, simulate_going_private.run_if(in_state(OpenToLANState::GoingPrivate)))
-        
-        .add_systems(Update, simulate_connecting.run_if(in_state(ConnectToServerState::Connecting)))
-        .add_systems(Update, simulate_disconnecting.run_if(in_state(ConnectToServerState::Disconnecting)))
-
-        // --- Observers ---
-        .add_observer(on_start_singleplayer)
-        .add_observer(on_stop_singleplayer)
-        .add_observer(on_lan_going_public)
-        .add_observer(on_lan_going_private)
-        .add_observer(on_start_connection)
-        .add_observer(on_disconnect_from_server)
-        .add_observer(on_retry_connection) // Neu: Retry Button
-        .add_observer(on_reset_to_menu)    // Neu: Error bestätigen
         .run()
 }
 
 fn setup_camera_system(mut commands: Commands) {
     commands.spawn(Camera2d);
-}
-
-#[derive(Resource, Default)]
-struct FakeLoadingTimer(Timer);
-
-impl FakeLoadingTimer {
-    fn start(&mut self, seconds: f32) {
-        self.0 = Timer::from_seconds(seconds, TimerMode::Once);
-        self.0.reset();
-    }
-}
-
-#[derive(Resource, Default)]
-struct ErrorMessage(String);
-
-// --- STATES ---
-
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, States)]
-enum AppScope {
-    #[default]
-    MainMenu,
-    Singleplayer,
-    Client,
-}
-
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, SubStates)]
-#[source(AppScope = AppScope::Singleplayer)]
-enum SingleplayerState {
-    #[default]
-    Starting, 
-    Running,
-    Closing,
-    Failed,
-}
-
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, SubStates)]
-#[source(AppScope = AppScope::Singleplayer)]
-enum OpenToLANState {
-    #[default]
-    Private,
-    GoingPublic,
-    Public,
-    GoingPrivate,
-    Failed,
-}
-
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, SubStates)]
-#[source(AppScope = AppScope::Client)]
-enum ConnectToServerState {
-    #[default]
-    Connecting,
-    Connected,
-    Disconnecting,
-    Failed,
 }
 
 // --- UI SYSTEM ---
@@ -108,21 +25,25 @@ fn ui_example_system(
     connect_state: Option<Res<State<ConnectToServerState>>>,
     error_msg: Res<ErrorMessage>,
 ) -> Result<(), bevy::prelude::BevyError> {
-    egui::Window::new("Host Client Status").show(egui.ctx_mut()?, |ui| {
-        match app_scope.get() {
-            AppScope::MainMenu => ui_main_menu(ui, &mut commands),
-            
-            AppScope::Singleplayer => {
-                if let (Some(sp_state), Some(lan_state)) = (singleplayer_state, open_to_lan_state) {
-                    ui_singleplayer(ui, &mut commands, sp_state.get(), lan_state.get(), &error_msg.0);
-                }
-            },
-            
-            AppScope::Client => {
-                if let Some(conn_state) = connect_state {
-                    ui_client(ui, &mut commands, conn_state.get(), &error_msg.0);
-                }
-            },
+    egui::Window::new("Host Client Status").show(egui.ctx_mut()?, |ui| match app_scope.get() {
+        AppScope::MainMenu => ui_main_menu(ui, &mut commands),
+
+        AppScope::Singleplayer => {
+            if let (Some(sp_state), Some(lan_state)) = (singleplayer_state, open_to_lan_state) {
+                ui_singleplayer(
+                    ui,
+                    &mut commands,
+                    sp_state.get(),
+                    lan_state.get(),
+                    &error_msg.0,
+                );
+            }
+        }
+
+        AppScope::Client => {
+            if let Some(conn_state) = connect_state {
+                ui_client(ui, &mut commands, conn_state.get(), &error_msg.0);
+            }
         }
     });
     Ok(())
@@ -139,9 +60,9 @@ fn ui_main_menu(ui: &mut egui::Ui, commands: &mut Commands) {
 }
 
 fn ui_singleplayer(
-    ui: &mut egui::Ui, 
-    commands: &mut Commands, 
-    sp_state: &SingleplayerState, 
+    ui: &mut egui::Ui,
+    commands: &mut Commands,
+    sp_state: &SingleplayerState,
     lan_state: &OpenToLANState,
     error_text: &str,
 ) {
@@ -165,13 +86,13 @@ fn ui_singleplayer(
         }
         SingleplayerState::Running => {
             ui.separator();
-            
+
             // LAN Status Behandlung
             if *lan_state == OpenToLANState::Failed {
                 ui.colored_label(egui::Color32::RED, format!("LAN Error: {}", error_text));
                 if ui.button("Acknowledge (Reset to Private)").clicked() {
                     // Wir resetten den LAN Status, aber bleiben im Singleplayer
-                    commands.trigger(SingleplayerGoingPrivate); 
+                    commands.trigger(SingleplayerGoingPrivate);
                 }
             } else {
                 ui.label(format!("LAN: {:?}", lan_state));
@@ -182,7 +103,10 @@ fn ui_singleplayer(
                         }
                     }
                     OpenToLANState::GoingPublic => {
-                        ui.horizontal(|ui| { ui.spinner(); ui.label("Opening Ports..."); });
+                        ui.horizontal(|ui| {
+                            ui.spinner();
+                            ui.label("Opening Ports...");
+                        });
                     }
                     OpenToLANState::Public => {
                         ui.label("Server is visible on LAN");
@@ -191,12 +115,15 @@ fn ui_singleplayer(
                         }
                     }
                     OpenToLANState::GoingPrivate => {
-                        ui.horizontal(|ui| { ui.spinner(); ui.label("Closing Ports..."); });
+                        ui.horizontal(|ui| {
+                            ui.spinner();
+                            ui.label("Closing Ports...");
+                        });
                     }
                     _ => {}
                 }
             }
-            
+
             ui.separator();
             if ui.button("Stop Singleplayer").clicked() {
                 commands.trigger(StopSingleplayer);
@@ -210,9 +137,14 @@ fn ui_singleplayer(
     }
 }
 
-fn ui_client(ui: &mut egui::Ui, commands: &mut Commands, state: &ConnectToServerState, error_text: &str) {
+fn ui_client(
+    ui: &mut egui::Ui,
+    commands: &mut Commands,
+    state: &ConnectToServerState,
+    error_text: &str,
+) {
     ui.heading("Client Mode");
-    
+
     match state {
         ConnectToServerState::Connecting => {
             ui.spinner();
@@ -230,7 +162,10 @@ fn ui_client(ui: &mut egui::Ui, commands: &mut Commands, state: &ConnectToServer
             ui.label("Disconnecting...");
         }
         ConnectToServerState::Failed => {
-            ui.colored_label(egui::Color32::RED, format!("Connection Failed: {}", error_text));
+            ui.colored_label(
+                egui::Color32::RED,
+                format!("Connection Failed: {}", error_text),
+            );
             ui.horizontal(|ui| {
                 if ui.button("Retry").clicked() {
                     commands.trigger(RetryConnection);
@@ -240,142 +175,5 @@ fn ui_client(ui: &mut egui::Ui, commands: &mut Commands, state: &ConnectToServer
                 }
             });
         }
-    }
-}
-
-// --- EVENTS ---
-
-#[derive(Event, Debug, Clone, Copy)] struct StartSingleplayer;
-#[derive(Event, Debug, Clone, Copy)] struct StopSingleplayer;
-#[derive(Event, Debug, Clone, Copy)] struct SingleplayerGoingPublic;
-#[derive(Event, Debug, Clone, Copy)] struct SingleplayerGoingPrivate;
-#[derive(Event, Debug, Clone, Copy)] struct StartConnection;
-#[derive(Event, Debug, Clone, Copy)] struct DisconnectFromServer;
-#[derive(Event, Debug, Clone, Copy)] struct RetryConnection;
-#[derive(Event, Debug, Clone, Copy)] struct ResetToMainMenu;
-
-// --- OBSERVERS ---
-
-fn on_start_singleplayer(_: On<StartSingleplayer>, mut scope: ResMut<NextState<AppScope>>, mut timer: ResMut<FakeLoadingTimer>) {
-    scope.set(AppScope::Singleplayer);
-    timer.start(1.5);
-}
-
-fn on_stop_singleplayer(_: On<StopSingleplayer>, mut sp_state: ResMut<NextState<SingleplayerState>>, mut timer: ResMut<FakeLoadingTimer>) {
-    sp_state.set(SingleplayerState::Closing);
-    timer.start(1.0);
-}
-
-fn on_lan_going_public(_: On<SingleplayerGoingPublic>, mut lan_state: ResMut<NextState<OpenToLANState>>, mut timer: ResMut<FakeLoadingTimer>) {
-    lan_state.set(OpenToLANState::GoingPublic);
-    timer.start(1.0);
-}
-
-fn on_lan_going_private(_: On<SingleplayerGoingPrivate>, mut lan_state: ResMut<NextState<OpenToLANState>>, mut timer: ResMut<FakeLoadingTimer>) {
-    lan_state.set(OpenToLANState::GoingPrivate);
-    timer.start(0.5);
-}
-
-fn on_start_connection(_: On<StartConnection>, mut scope: ResMut<NextState<AppScope>>, mut timer: ResMut<FakeLoadingTimer>) {
-    scope.set(AppScope::Client);
-    timer.start(2.0);
-}
-
-fn on_disconnect_from_server(_: On<DisconnectFromServer>, mut conn_state: ResMut<NextState<ConnectToServerState>>, mut timer: ResMut<FakeLoadingTimer>) {
-    conn_state.set(ConnectToServerState::Disconnecting);
-    timer.start(0.5);
-}
-
-fn on_retry_connection(_: On<RetryConnection>, mut conn_state: ResMut<NextState<ConnectToServerState>>, mut timer: ResMut<FakeLoadingTimer>) {
-    // Reset state to Connecting -> triggers simulation again
-    conn_state.set(ConnectToServerState::Connecting);
-    timer.start(1.0);
-}
-
-fn on_reset_to_menu(_: On<ResetToMainMenu>, mut scope: ResMut<NextState<AppScope>>) {
-    scope.set(AppScope::MainMenu);
-}
-
-// --- SIMULATION SYSTEMS (mit Zufallsfehlern) ---
-
-fn simulate_singleplayer_starting(
-    time: Res<Time>, 
-    mut timer: ResMut<FakeLoadingTimer>, 
-    mut next: ResMut<NextState<SingleplayerState>>,
-    mut err: ResMut<ErrorMessage>
-) {
-    timer.0.tick(time.delta());
-    if timer.0.is_finished() {
-        // 10% Chance auf Fehler
-        if rand::thread_rng().gen_bool(0.1) {
-            err.0 = "Failed to bind port 8080".to_string();
-            next.set(SingleplayerState::Failed);
-        } else {
-            next.set(SingleplayerState::Running);
-        }
-    }
-}
-
-fn simulate_singleplayer_closing(
-    time: Res<Time>, 
-    mut timer: ResMut<FakeLoadingTimer>, 
-    mut next_scope: ResMut<NextState<AppScope>>,
-    mut next_lan: ResMut<NextState<OpenToLANState>> 
-) {
-    timer.0.tick(time.delta());
-    next_lan.set(OpenToLANState::Private);
-
-    if timer.0.is_finished() {
-        next_scope.set(AppScope::MainMenu);
-    }
-}
-
-fn simulate_going_public(
-    time: Res<Time>, 
-    mut timer: ResMut<FakeLoadingTimer>, 
-    mut next: ResMut<NextState<OpenToLANState>>,
-    mut err: ResMut<ErrorMessage>
-) {
-    timer.0.tick(time.delta());
-    if timer.0.is_finished() {
-         // 20% Chance auf Fehler (z.B. Port schon belegt)
-         if rand::thread_rng().gen_bool(0.2) {
-            err.0 = "UPnP Negotiation Failed".to_string();
-            next.set(OpenToLANState::Failed);
-        } else {
-            next.set(OpenToLANState::Public);
-        }
-    }
-}
-
-fn simulate_going_private(time: Res<Time>, mut timer: ResMut<FakeLoadingTimer>, mut next: ResMut<NextState<OpenToLANState>>) {
-    timer.0.tick(time.delta());
-    if timer.0.is_finished() {
-        next.set(OpenToLANState::Private);
-    }
-}
-
-fn simulate_connecting(
-    time: Res<Time>, 
-    mut timer: ResMut<FakeLoadingTimer>, 
-    mut next: ResMut<NextState<ConnectToServerState>>,
-    mut err: ResMut<ErrorMessage>
-) {
-    timer.0.tick(time.delta());
-    if timer.0.is_finished() {
-        // 30% Chance auf Connection Error
-        if rand::thread_rng().gen_bool(0.3) {
-            err.0 = "Host unreachable (Timeout)".to_string();
-            next.set(ConnectToServerState::Failed);
-        } else {
-            next.set(ConnectToServerState::Connected);
-        }
-    }
-}
-
-fn simulate_disconnecting(time: Res<Time>, mut timer: ResMut<FakeLoadingTimer>, mut next_scope: ResMut<NextState<AppScope>>) {
-    timer.0.tick(time.delta());
-    if timer.0.is_finished() {
-        next_scope.set(AppScope::MainMenu);
     }
 }
--- fos_server/src/lib.rs
+++ fos_server/src/lib.rs
@@ -1,3 +1,284 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::prelude::*;
+use rand::Rng;
+
+pub struct FOSServerPlugin;
+
+impl Plugin for FOSServerPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_state::<AppScope>()
+            .init_resource::<FakeLoadingTimer>()
+            .init_resource::<ErrorMessage>()
+            .add_sub_state::<SingleplayerState>()
+            .add_sub_state::<OpenToLANState>()
+            .add_sub_state::<ConnectToServerState>()
+            // --- Simulation Logic ---
+            .add_systems(
+                Update,
+                simulate_singleplayer_starting.run_if(in_state(SingleplayerState::Starting)),
+            )
+            .add_systems(
+                Update,
+                simulate_singleplayer_closing.run_if(in_state(SingleplayerState::Closing)),
+            )
+            .add_systems(
+                Update,
+                simulate_going_public.run_if(in_state(OpenToLANState::GoingPublic)),
+            )
+            .add_systems(
+                Update,
+                simulate_going_private.run_if(in_state(OpenToLANState::GoingPrivate)),
+            )
+            .add_systems(
+                Update,
+                simulate_connecting.run_if(in_state(ConnectToServerState::Connecting)),
+            )
+            .add_systems(
+                Update,
+                simulate_disconnecting.run_if(in_state(ConnectToServerState::Disconnecting)),
+            )
+            // --- Observers ---
+            .add_observer(on_start_singleplayer)
+            .add_observer(on_stop_singleplayer)
+            .add_observer(on_lan_going_public)
+            .add_observer(on_lan_going_private)
+            .add_observer(on_start_connection)
+            .add_observer(on_disconnect_from_server)
+            .add_observer(on_retry_connection)
+            .add_observer(on_reset_to_menu);
+    }
+}
+
+#[derive(Resource, Default)]
+pub struct FakeLoadingTimer(Timer);
+
+impl FakeLoadingTimer {
+    fn start(&mut self, seconds: f32) {
+        self.0 = Timer::from_seconds(seconds, TimerMode::Once);
+        self.0.reset();
+    }
+}
+
+#[derive(Resource, Default)]
+pub struct ErrorMessage(pub String);
+
+// --- STATES ---
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, States)]
+pub enum AppScope {
+    #[default]
+    MainMenu,
+    Singleplayer,
+    Client,
+}
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, SubStates)]
+#[source(AppScope = AppScope::Singleplayer)]
+pub enum SingleplayerState {
+    #[default]
+    Starting,
+    Running,
+    Closing,
+    Failed,
+}
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, SubStates)]
+#[source(AppScope = AppScope::Singleplayer)]
+pub enum OpenToLANState {
+    #[default]
+    Private,
+    GoingPublic,
+    Public,
+    GoingPrivate,
+    Failed,
+}
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, SubStates)]
+#[source(AppScope = AppScope::Client)]
+pub enum ConnectToServerState {
+    #[default]
+    Connecting,
+    Connected,
+    Disconnecting,
+    Failed,
+}
+
+// --- EVENTS ---
+
+#[derive(Event, Debug, Clone, Copy)]
+pub struct StartSingleplayer;
+#[derive(Event, Debug, Clone, Copy)]
+pub struct StopSingleplayer;
+#[derive(Event, Debug, Clone, Copy)]
+pub struct SingleplayerGoingPublic;
+#[derive(Event, Debug, Clone, Copy)]
+pub struct SingleplayerGoingPrivate;
+#[derive(Event, Debug, Clone, Copy)]
+pub struct StartConnection;
+#[derive(Event, Debug, Clone, Copy)]
+pub struct DisconnectFromServer;
+#[derive(Event, Debug, Clone, Copy)]
+pub struct RetryConnection;
+#[derive(Event, Debug, Clone, Copy)]
+pub struct ResetToMainMenu;
+
+// --- OBSERVERS ---
+
+pub fn on_start_singleplayer(
+    _: On<StartSingleplayer>,
+    mut scope: ResMut<NextState<AppScope>>,
+    mut timer: ResMut<FakeLoadingTimer>,
+) {
+    scope.set(AppScope::Singleplayer);
+    timer.start(1.5);
+}
+
+pub fn on_stop_singleplayer(
+    _: On<StopSingleplayer>,
+    mut sp_state: ResMut<NextState<SingleplayerState>>,
+    mut timer: ResMut<FakeLoadingTimer>,
+) {
+    sp_state.set(SingleplayerState::Closing);
+    timer.start(1.0);
+}
+
+pub fn on_lan_going_public(
+    _: On<SingleplayerGoingPublic>,
+    mut lan_state: ResMut<NextState<OpenToLANState>>,
+    mut timer: ResMut<FakeLoadingTimer>,
+) {
+    lan_state.set(OpenToLANState::GoingPublic);
+    timer.start(1.0);
+}
+
+pub fn on_lan_going_private(
+    _: On<SingleplayerGoingPrivate>,
+    mut lan_state: ResMut<NextState<OpenToLANState>>,
+    mut timer: ResMut<FakeLoadingTimer>,
+) {
+    lan_state.set(OpenToLANState::GoingPrivate);
+    timer.start(0.5);
+}
+
+pub fn on_start_connection(
+    _: On<StartConnection>,
+    mut scope: ResMut<NextState<AppScope>>,
+    mut timer: ResMut<FakeLoadingTimer>,
+) {
+    scope.set(AppScope::Client);
+    timer.start(2.0);
+}
+
+pub fn on_disconnect_from_server(
+    _: On<DisconnectFromServer>,
+    mut conn_state: ResMut<NextState<ConnectToServerState>>,
+    mut timer: ResMut<FakeLoadingTimer>,
+) {
+    conn_state.set(ConnectToServerState::Disconnecting);
+    timer.start(0.5);
+}
+
+pub fn on_retry_connection(
+    _: On<RetryConnection>,
+    mut conn_state: ResMut<NextState<ConnectToServerState>>,
+    mut timer: ResMut<FakeLoadingTimer>,
+) {
+    // Reset state to Connecting -> triggers simulation again
+    conn_state.set(ConnectToServerState::Connecting);
+    timer.start(1.0);
+}
+
+pub fn on_reset_to_menu(_: On<ResetToMainMenu>, mut scope: ResMut<NextState<AppScope>>) {
+    scope.set(AppScope::MainMenu);
+}
+
+// --- SIMULATION SYSTEMS (mit Zufallsfehlern) ---
+
+pub fn simulate_singleplayer_starting(
+    time: Res<Time>,
+    mut timer: ResMut<FakeLoadingTimer>,
+    mut next: ResMut<NextState<SingleplayerState>>,
+    mut err: ResMut<ErrorMessage>,
+) {
+    timer.0.tick(time.delta());
+    if timer.0.is_finished() {
+        // 10% Chance auf Fehler
+        if rand::thread_rng().gen_bool(0.1) {
+            err.0 = "Failed to bind port 8080".to_string();
+            next.set(SingleplayerState::Failed);
+        } else {
+            next.set(SingleplayerState::Running);
+        }
+    }
+}
+
+pub fn simulate_singleplayer_closing(
+    time: Res<Time>,
+    mut timer: ResMut<FakeLoadingTimer>,
+    mut next_scope: ResMut<NextState<AppScope>>,
+    mut next_lan: ResMut<NextState<OpenToLANState>>,
+) {
+    timer.0.tick(time.delta());
+    next_lan.set(OpenToLANState::Private);
+
+    if timer.0.is_finished() {
+        next_scope.set(AppScope::MainMenu);
+    }
+}
+
+pub fn simulate_going_public(
+    time: Res<Time>,
+    mut timer: ResMut<FakeLoadingTimer>,
+    mut next: ResMut<NextState<OpenToLANState>>,
+    mut err: ResMut<ErrorMessage>,
+) {
+    timer.0.tick(time.delta());
+    if timer.0.is_finished() {
+        // 20% Chance auf Fehler (z.B. Port schon belegt)
+        if rand::thread_rng().gen_bool(0.2) {
+            err.0 = "UPnP Negotiation Failed".to_string();
+            next.set(OpenToLANState::Failed);
+        } else {
+            next.set(OpenToLANState::Public);
+        }
+    }
+}
+
+pub fn simulate_going_private(
+    time: Res<Time>,
+    mut timer: ResMut<FakeLoadingTimer>,
+    mut next: ResMut<NextState<OpenToLANState>>,
+) {
+    timer.0.tick(time.delta());
+    if timer.0.is_finished() {
+        next.set(OpenToLANState::Private);
+    }
+}
+
+pub fn simulate_connecting(
+    time: Res<Time>,
+    mut timer: ResMut<FakeLoadingTimer>,
+    mut next: ResMut<NextState<ConnectToServerState>>,
+    mut err: ResMut<ErrorMessage>,
+) {
+    timer.0.tick(time.delta());
+    if timer.0.is_finished() {
+        // 30% Chance auf Connection Error
+        if rand::thread_rng().gen_bool(0.3) {
+            err.0 = "Host unreachable (Timeout)".to_string();
+            next.set(ConnectToServerState::Failed);
+        } else {
+            next.set(ConnectToServerState::Connected);
+        }
+    }
+}
+
+pub fn simulate_disconnecting(
+    time: Res<Time>,
+    mut timer: ResMut<FakeLoadingTimer>,
+    mut next_scope: ResMut<NextState<AppScope>>,
+) {
+    timer.0.tick(time.delta());
+    if timer.0.is_finished() {
+        next_scope.set(AppScope::MainMenu);
+    }
 }
