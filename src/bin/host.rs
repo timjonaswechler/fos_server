2c518b591a09c842c93080969001d03f3c725726
Tim-Jonas Wechler <code.wechlertimjonas@icloud.com>
2025-12-10 13:52:06 +0100

    expaned UI System

--- src/bin/host.rs
+++ src/bin/host.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin, EguiPrimaryContextPass};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use fos_server::{client::events::*, server::events::*, singleplayer::events::*, states::*, *};
+use fos_server::{states::*, *};
 
 fn main() -> AppExit {
     App::new()
@@ -13,7 +13,28 @@ fn main() -> AppExit {
         ))
         .insert_resource(UI)
         .add_systems(Startup, setup_camera_system)
-        .add_systems(EguiPrimaryContextPass, ui_example_system)
+        .add_systems(
+            EguiPrimaryContextPass,
+            ui_menu_system.run_if(in_state(AppScope::Menu)),
+        )
+        .add_systems(
+            EguiPrimaryContextPass,
+            ui_singleplayer_system
+                .run_if(in_state(AppScope::InGame))
+                .run_if(in_state(GameMode::Singleplayer)),
+        )
+        .add_systems(
+            EguiPrimaryContextPass,
+            ui_client_system
+                .run_if(in_state(AppScope::InGame))
+                .run_if(in_state(GameMode::Client)),
+        )
+        .add_systems(
+            EguiPrimaryContextPass,
+            ui_game_menu
+                .run_if(in_state(AppScope::InGame))
+                .run_if(in_state(InGameMode::GameMenu)),
+        )
         .run()
 }
 
@@ -26,84 +47,163 @@ pub struct UI;
 
 // --- UI SYSTEM ---
 
-fn ui_example_system(
+fn ui_singleplayer_system(
     mut commands: Commands,
     mut egui: EguiContexts,
     app_state: Res<State<AppScope>>,
-    menu_state: Res<State<MenuState>>,
-    singleplayer_menu_state: Option<Res<State<SingleplayerMenuState>>>,
-    multiplayer_menu_state: Option<Res<State<MultiplayerMenuState>>>,
-    // wiki_menu_state: Res<State<WikiMenuState>>,
-    // settings_menu_state: Res<State<SettingsMenuState>>,
+    game_mode_state: Res<State<GameMode>>,
+    singleplayer_state: Res<State<SingleplayerState>>,
 ) -> Result<(), bevy::prelude::BevyError> {
-    egui::Window::new("APP").show(egui.ctx_mut()?, |ui| {
+    egui::Window::new("APP Game - Singleplayer").show(egui.ctx_mut()?, |ui| {
+        ui.vertical_centered_justified(|ui| match *app_state.get() {
+            AppScope::InGame => match *game_mode_state.get() {
+                GameMode::Singleplayer => match *singleplayer_state.get() {
+                    SingleplayerState::Running => {
+                        ui.label("Singleplayer is running");
+                        ui.separator();
+                    }
+                    _ => {
+                        ui.label("Singleplayer is not running");
+                    }
+                },
+                _ => {} // client
+            },
+            _ => {} // menu
+        });
+    });
+    Ok(())
+}
+
+fn ui_client_system(
+    mut commands: Commands,
+    mut egui: EguiContexts,
+    app_state: Res<State<AppScope>>,
+    game_mode_state: Res<State<GameMode>>,
+    singleplayer_menu_state: Option<Res<State<SingleplayerMenuScreen>>>,
+    multiplayer_menu_state: Option<Res<State<MultiplayerMenuScreen>>>,
+) -> Result<(), bevy::prelude::BevyError> {
+    egui::Window::new("APP Game - Client").show(egui.ctx_mut()?, |ui| {
+        ui.vertical_centered_justified(|ui| match *app_state.get() {
+            AppScope::InGame => match *game_mode_state.get() {
+                GameMode::Client => {
+                    ui.label("Hello");
+                }
+                _ => {} // singleplayer
+            },
+            _ => {} // menu
+        });
+    });
+    Ok(())
+}
+
+fn ui_game_menu(
+    mut commands: Commands,
+    mut egui: EguiContexts,
+    app_state: Res<State<AppScope>>,
+    game_mode_state: Res<State<GameMode>>,
+    in_game_mode_state: Res<State<InGameMode>>,
+) -> Result<(), bevy::prelude::BevyError> {
+    egui::Window::new("APP Game Menu").show(egui.ctx_mut()?, |ui| {
+        ui.vertical_centered_justified(|ui| match *app_state.get() {
+            AppScope::InGame => match *in_game_mode_state.get() {
+                InGameMode::GameMenu => {
+                    ui.label("Game Menu");
+                    ui.button("Back").clicked().then(|| {
+                        commands.trigger(SingleplayerStateEvent::RequestTransitionTo(
+                            SingleplayerState::Stopping,
+                        ));
+                    });
+                }
+                _ => {} // playing
+            },
+            _ => {} // menu
+        });
+    });
+    Ok(())
+}
+
+fn ui_menu_system(
+    mut commands: Commands,
+    mut egui: EguiContexts,
+    app_state: Res<State<AppScope>>,
+    menu_state: Res<State<MenuScreen>>,
+    singleplayer_menu_state: Option<Res<State<SingleplayerMenuScreen>>>,
+    multiplayer_menu_state: Option<Res<State<MultiplayerMenuScreen>>>,
+    // wiki_menu_state: Res<State<WikiMenuScreen>>,
+    // settings_menu_state: Res<State<SettingsMenuScreen>>,
+) -> Result<(), bevy::prelude::BevyError> {
+    egui::Window::new("APP Menu").show(egui.ctx_mut()?, |ui| {
         ui.vertical_centered_justified(|ui| match *app_state.get() {
             AppScope::Menu => match *menu_state.get() {
-                MenuState::Main => {
+                MenuScreen::Main => {
                     ui.vertical_centered_justified(|ui| {
                         if ui.button("Singleplayer").clicked() {
                             commands.trigger(MainMenuEvent::RequestTransitionTo(
-                                MenuState::Singleplayer,
+                                MenuScreen::Singleplayer,
                             ));
                         }
                         if ui.button("Multiplayer").clicked() {
                             commands.trigger(MainMenuEvent::RequestTransitionTo(
-                                MenuState::Multiplayer,
+                                MenuScreen::Multiplayer,
                             ));
                         }
                         if ui.button("Wiki").clicked() {
-                            commands.trigger(MainMenuEvent::RequestTransitionTo(MenuState::Wiki));
+                            commands.trigger(MainMenuEvent::RequestTransitionTo(MenuScreen::Wiki));
                         }
                         if ui.button("Settings").clicked() {
                             commands
-                                .trigger(MainMenuEvent::RequestTransitionTo(MenuState::Settings));
+                                .trigger(MainMenuEvent::RequestTransitionTo(MenuScreen::Settings));
                         }
                         if ui.add_enabled(false, egui::Button::new("Quit")).clicked() {
                             unreachable!();
                         }
                     });
                 }
-                MenuState::Singleplayer => {
+                MenuScreen::Singleplayer => {
                     ui.vertical_centered_justified(|ui| {
                         if let Some(singleplayer_menu_state) = singleplayer_menu_state.as_ref() {
                             match singleplayer_menu_state.get() {
-                                SingleplayerMenuState::Overview => {
+                                SingleplayerMenuScreen::Overview => {
                                     if ui.button("New Game").clicked() {
                                         commands.trigger(
                                             SingleplayerMenuEvent::RequestTransitionTo(
-                                                SingleplayerMenuState::NewGame,
+                                                SingleplayerMenuScreen::NewGame,
                                             ),
                                         );
                                     }
                                     if ui.button("Load Game").clicked() {
                                         commands.trigger(
                                             SingleplayerMenuEvent::RequestTransitionTo(
-                                                SingleplayerMenuState::LoadGame,
+                                                SingleplayerMenuScreen::LoadGame,
                                             ),
                                         );
                                     }
                                     if ui.button("Back").clicked() {
                                         commands.trigger(MainMenuEvent::RequestTransitionTo(
-                                            MenuState::Main,
+                                            MenuScreen::Main,
                                         ));
                                     }
                                 }
-                                SingleplayerMenuState::NewGame => {
-                                    if ui.button("Start").clicked() {}
+                                SingleplayerMenuScreen::NewGame => {
+                                    if ui.button("Start").clicked() {
+                                        commands.trigger(GameModeEvent::RequestTransitionTo(
+                                            GameMode::Singleplayer,
+                                        ));
+                                    }
                                     if ui.button("Back").clicked() {
                                         commands.trigger(
                                             SingleplayerMenuEvent::RequestTransitionTo(
-                                                SingleplayerMenuState::Overview,
+                                                SingleplayerMenuScreen::Overview,
                                             ),
                                         );
                                     }
                                 }
-                                SingleplayerMenuState::LoadGame => {
+                                SingleplayerMenuScreen::LoadGame => {
                                     if ui.button("Load").clicked() {}
                                     if ui.button("Back").clicked() {
                                         commands.trigger(
                                             SingleplayerMenuEvent::RequestTransitionTo(
-                                                SingleplayerMenuState::Overview,
+                                                SingleplayerMenuScreen::Overview,
                                             ),
                                         );
                                     }
@@ -112,81 +212,89 @@ fn ui_example_system(
                         }
                     });
                 }
-                MenuState::Multiplayer => {
+                MenuScreen::Multiplayer => {
                     ui.vertical_centered_justified(|ui| {
                         if let Some(multiplayer_menu_state) = multiplayer_menu_state.as_ref() {
                             match multiplayer_menu_state.get() {
-                                MultiplayerMenuState::Overview => {
+                                MultiplayerMenuScreen::Overview => {
                                     if ui.button("Host new Game").clicked() {
                                         commands.trigger(
                                             MultiplayerMenuEvent::RequestTransitionTo(
-                                                MultiplayerMenuState::HostNewGame,
+                                                MultiplayerMenuScreen::HostNewGame,
                                             ),
                                         );
                                     }
                                     if ui.button("Host saved Game").clicked() {
                                         commands.trigger(
                                             MultiplayerMenuEvent::RequestTransitionTo(
-                                                MultiplayerMenuState::HostNewGame,
+                                                MultiplayerMenuScreen::HostNewGame,
                                             ),
                                         );
                                     }
                                     if ui.button("Join public Game").clicked() {
                                         commands.trigger(
                                             MultiplayerMenuEvent::RequestTransitionTo(
-                                                MultiplayerMenuState::JoinPublicGame,
+                                                MultiplayerMenuScreen::JoinPublicGame,
                                             ),
                                         );
                                     }
                                     if ui.button("Join local Game").clicked() {
                                         commands.trigger(
                                             MultiplayerMenuEvent::RequestTransitionTo(
-                                                MultiplayerMenuState::JoinLocalGame,
+                                                MultiplayerMenuScreen::JoinLocalGame,
                                             ),
                                         );
                                     }
                                     if ui.button("Back").clicked() {
                                         commands.trigger(MainMenuEvent::RequestTransitionTo(
-                                            MenuState::Main,
+                                            MenuScreen::Main,
                                         ));
                                     }
                                 }
-                                MultiplayerMenuState::HostNewGame => {
+                                MultiplayerMenuScreen::HostNewGame => {
                                     if ui.button("New Game").clicked() {}
                                     if ui.button("Back").clicked() {
                                         commands.trigger(
                                             MultiplayerMenuEvent::RequestTransitionTo(
-                                                MultiplayerMenuState::Overview,
+                                                MultiplayerMenuScreen::Overview,
                                             ),
                                         );
                                     }
                                 }
-                                MultiplayerMenuState::HostSavedGame => {
+                                MultiplayerMenuScreen::HostSavedGame => {
                                     if ui.button("Load Game").clicked() {}
                                     if ui.button("Back").clicked() {
                                         commands.trigger(
                                             MultiplayerMenuEvent::RequestTransitionTo(
-                                                MultiplayerMenuState::Overview,
+                                                MultiplayerMenuScreen::Overview,
                                             ),
                                         );
                                     }
                                 }
-                                MultiplayerMenuState::JoinPublicGame => {
-                                    if ui.button("Join Public Game").clicked() {}
+                                MultiplayerMenuScreen::JoinPublicGame => {
+                                    if ui.button("Join Public Game").clicked() {
+                                        commands.trigger(GameModeEvent::RequestTransitionTo(
+                                            GameMode::Client,
+                                        ));
+                                    }
                                     if ui.button("Back").clicked() {
                                         commands.trigger(
                                             MultiplayerMenuEvent::RequestTransitionTo(
-                                                MultiplayerMenuState::Overview,
+                                                MultiplayerMenuScreen::Overview,
                                             ),
                                         );
                                     }
                                 }
-                                MultiplayerMenuState::JoinLocalGame => {
-                                    if ui.button("Join Local Game").clicked() {}
+                                MultiplayerMenuScreen::JoinLocalGame => {
+                                    if ui.button("Join Local Game").clicked() {
+                                        commands.trigger(GameModeEvent::RequestTransitionTo(
+                                            GameMode::Client,
+                                        ));
+                                    }
                                     if ui.button("Back").clicked() {
                                         commands.trigger(
                                             MultiplayerMenuEvent::RequestTransitionTo(
-                                                MultiplayerMenuState::Overview,
+                                                MultiplayerMenuScreen::Overview,
                                             ),
                                         );
                                     }
@@ -195,17 +303,17 @@ fn ui_example_system(
                         }
                     });
                 }
-                MenuState::Wiki => {
+                MenuScreen::Wiki => {
                     ui.vertical_centered_justified(|ui| {
                         if ui.button("Back").clicked() {
-                            commands.trigger(MainMenuEvent::RequestTransitionTo(MenuState::Main));
+                            commands.trigger(MainMenuEvent::RequestTransitionTo(MenuScreen::Main));
                         }
                     });
                 }
-                MenuState::Settings => {
+                MenuScreen::Settings => {
                     ui.vertical_centered_justified(|ui| {
                         if ui.button("Back").clicked() {
-                            commands.trigger(MainMenuEvent::RequestTransitionTo(MenuState::Main));
+                            commands.trigger(MainMenuEvent::RequestTransitionTo(MenuScreen::Main));
                         }
                     });
                 }
